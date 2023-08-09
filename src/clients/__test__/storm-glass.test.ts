98352c53a4148ef4cf5ec15a2bb8a5cf8e5c7fbd
CaiqueRibeiro <ribeiro.caique95@gmail.com>
2023-08-08 23:22:34 -0300

    chore: adding generics to abstract axios

--- src/clients/__test__/storm-glass.test.ts
+++ src/clients/__test__/storm-glass.test.ts
@@ -1,21 +1,66 @@
-import axios from 'axios'
+import * as HTTPUtil from '@src/util/request'
 import { StormGlass } from '@src/clients/storm-glass'
 import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json'
 import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json'
 
-jest.mock('axios')
+jest.mock('@src/util/request')
 
 describe('StormGlass client', () => {
-  const mockedAxios = axios as jest.Mocked<typeof axios>
+  const MockedRequestClass = HTTPUtil.Request as jest.Mocked<typeof HTTPUtil.Request>
+  const mockedRequest = new HTTPUtil.Request() as jest.Mocked<HTTPUtil.Request>
 
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726
     const lng = 151.289824
-
-    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture })
-
-    const stormGlass = new StormGlass(mockedAxios)
+    mockedRequest.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture } as HTTPUtil.Response)
+    const stormGlass = new StormGlass(mockedRequest)
     const response = await stormGlass.fetchPoints(lat, lng)
     expect(response).toEqual(stormGlassNormalized3HoursFixture)
-  })
+  });
+
+  it('should exclude incomplete data points', async () => {
+    const lat = -33.792726
+    const lng = 151.289824
+    const incompleteDataPoints = {
+      hours: [
+        {
+          windDirections: {
+            noaa: 300,
+          },
+          time: '2022-04-26T00:00:00+00:00'
+        }
+      ]
+    }
+    mockedRequest.get.mockResolvedValue({ data: incompleteDataPoints } as HTTPUtil.Response)
+    const stormGlass = new StormGlass(mockedRequest)
+    const response = await stormGlass.fetchPoints(lat, lng)
+    expect(response).toEqual([])
+  });
+
+  it('should get a generic error from StormGlass service when the request fail before reaching the service', async () => {
+    const lat = -33.792726
+    const lng = 151.289824
+    MockedRequestClass.isRequestError.mockReturnValue(false)
+    mockedRequest.get.mockRejectedValue({ message: 'Network Error' })
+    const stormGlass = new StormGlass(mockedRequest)
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error when trying to communicate to StormGlass: Network Error'
+    )
+  });
+
+  it('should get an StormGlassResponseError when the StormGlass service resopnds with error', async () => {
+    const lat = -33.792726
+    const lng = 151.289824
+    MockedRequestClass.isRequestError.mockReturnValue(true)
+    mockedRequest.get.mockRejectedValue({
+      response: {
+        status: 429,
+        data: { errors: ['Rate Limit reached' ] }
+      }
+    })
+    const stormGlass = new StormGlass(mockedRequest)
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error returned by StormGlass service. Error: {"errors":["Rate Limit reached"]} Code: 429'
+    )
+  });
 })
--- src/clients/storm-glass.ts
+++ src/clients/storm-glass.ts
@@ -1,4 +1,6 @@
-import { AxiosStatic } from "axios"
+import { InternalError } from "@src/util/errors/internal-error";
+import config, { IConfig } from 'config'
+import * as HTTPUtil from '@src/util/request'
 
 export interface StormGlassPointSource {
   [key: string]: number;
@@ -30,15 +32,55 @@ export interface ForecastPoint {
   windSpeed: number;
 }
 
+export class ClientRequestError extends InternalError {
+  constructor(message: string) {
+    const internalMessage = 'Unexpected error when trying to communicate to StormGlass'
+    super(`${internalMessage}: ${message}`)
+  }
+}
+
+export class StormGlassResponseError extends InternalError {
+  constructor(message: string) {
+    const internalMessage = 'Unexpected error returned by StormGlass service'
+    super(`${internalMessage}. ${message}`)
+  }
+}
+
+const stormGlassResourceConfig: IConfig = config.get('App.resources.StormGlass')
+
 export class StormGlass {
   readonly stormGlassAPIParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed'
   readonly stormGlassAPISource = 'noaa'
 
-  constructor(protected request: AxiosStatic) {}
+  constructor(protected request = new HTTPUtil.Request()) {}
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
-    const response = await this.request.get<StormGlassForecastResponse>(`https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`)
-    return this.normalizeResponse(response.data)
+    try {
+      const response = await this.request.get<StormGlassForecastResponse>(
+        `${stormGlassResourceConfig.get(
+          'apiUrl'
+          )}/weather/point?params=${this.stormGlassAPIParams
+          }&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`,
+      {
+        headers: {
+          Authorization: stormGlassResourceConfig.get('apiToken')
+        }
+      })
+      return this.normalizeResponse(response.data)
+    } catch (error) {
+      /**
+       * This is handling the Axios errors specifically
+       */
+      if (HTTPUtil.Request.isRequestError(error as Error)) {
+        const axiosError = error as HTTPUtil.RequestError
+        throw new StormGlassResponseError(
+          `Error: ${JSON.stringify(axiosError.response?.data)} Code: ${
+            axiosError.response?.status
+          }`
+        );
+      }
+      throw new ClientRequestError((error as Error).message);
+    }
   }
 
   private normalizeResponse(points: StormGlassForecastResponse): ForecastPoint[] {
